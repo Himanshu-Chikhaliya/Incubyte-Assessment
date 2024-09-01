f68983e7146eaa4676ce2e315c90024054c4281d
Himanshu-Chikhaliya <chikhaliyahk@gmail.com>
2024-09-01 13:30:53 +0530

    Added failing test for validate Book

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -13,4 +13,19 @@ public class BookTest {
         assertEquals(Year.of(1910),book.publicationYear);
     }
 
+    @Test
+    void bookDetailsWithNullAndEmpty(){
+        Book book1 = new Book("","Gitanjali","Rabindranath Tagore",Year.of(1910));
+        assertFalse(book1.isValid());
+
+        Book book2 = new Book("9780684839349","","Rabindranath Tagore",Year.of(1910));
+        assertFalse(book2.isValid());
+
+        Book book3 = new Book("9780684839349","Gitanjali","",Year.of(1910));
+        assertFalse(book3.isValid());
+
+        Book book4 = new Book("9780684839349","Gitanjali","Rabindranath Tagore",null);
+        assertFalse(book4.isValid());
+    }
+
 }
