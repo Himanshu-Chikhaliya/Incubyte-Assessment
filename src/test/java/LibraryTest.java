bd77505c265d9463df1f96db1859b21dd6248ab5
Himanshu-Chikhaliya <chikhaliyahk@gmail.com>
2024-09-01 16:49:27 +0530

    Added failing tests for add book functionality

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -18,6 +21,30 @@ public class LibraryTest {
         Exception ex2 = assertThrows(Exception.class, library2::isValidName);
         assertEquals("Please Enter library name", ex2.getMessage());
 
+    }
+
+
+
+    /* --------------------- Test cases for Add Book  -------------------- */
+    @Test
+    void TheBookDetailsAreNotComplete(){
+        Book book1 = new Book("","Vevishal","zaverchand meghani", Year.of(1955));
+        Exception ex1 = assertThrows(Exception.class, () -> library.addBook(book1));
+        assertEquals("Please fill all the details", ex1.getMessage());
+    }
+
+    @Test
+    void allDetailsAreCompleted(){
+        Book book2 = new Book("9781981876037","Vevishal","zaverchand meghani",Year.of(1955));
+        assertDoesNotThrow(() -> library.addBook(book2));
+    }
+
+    @Test
+    void bookIsAlreadyAdded() throws Exception {
+        Book book2 = new Book("9781981876037","Vevishal","zaverchand meghani",Year.of(1955));
+        library.addBook(book2);
+        Exception ex2 = assertThrows(Exception.class, () -> library.addBook(book2));
+        assertEquals("A book has already added",ex2.getMessage());
     }
 
 }
